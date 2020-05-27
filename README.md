35b76735b38ea2c0ee8d43a011a3ec132d77bfac
Chubanova <m.esaeva@inbox.ru>
2020-05-27 18:39:40 +0300

    work in progress

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # mailer
+
 A tool for sending messages over REST API
 
-# Requirements:
+## Requirements
+
 JRE >= 11
 RabbitMQ
 PostgreSQL >= 9.5
 
+## Docker commands for RabbitMQ and PostgreSQL
+
+### Just for development and testing purposes
+
+docker run -d --hostname my-rabbit --name some-rabbit -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker run -d -e POSTGRES_PASSWORD=11 -p 5432:5432 postgres
--- rest/src/main/java/io/mailer/rest/rpc/EmailSenderRpcClient.java
+++ rest/src/main/java/io/mailer/rest/rpc/EmailSenderRpcClient.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -21,55 +22,64 @@ public class EmailSenderRpcClient {
 
     private static final ObjectMapper mapper = new ObjectMapper();
     private static final Logger LOG = LoggerFactory.getLogger(EmailSenderRpcClient.class);
+    private final ConnectionFactory factory;
     
     private static final String QUEUE = "MAILER_EMAIL";
 
-    @Value("${amqp.server}")
-    private String server;
-
-    @Value("${amqp.user}")
-    private String user;
-
-    @Value("${amqp.password}")
-    private String password;
-
-    public Object exchange(EmailRequest req) {
-
-        Object result = null;
-
-        var factory = new ConnectionFactory();
+    public EmailSenderRpcClient(
+            @Value("${amqp.server}") String server,
+            @Value("${amqp.user}") String user,
+            @Value("${amqp.password}") String password
+            ) throws Exception {
+                
+        factory = new ConnectionFactory();
         factory.setHost(server);
         factory.setUsername(user);
         factory.setPassword(password);
 
         try (var conn = factory.newConnection();
             var channel = conn.createChannel()) {
-                
-                final var correlationId = UUID.randomUUID().toString();
-                final var replyTo = channel.queueDeclare().getQueue();
-                
-                var props = new AMQP.BasicProperties().builder()
-                    .correlationId(correlationId)
-                    .replyTo(replyTo)
-                    .build();
 
-                channel.basicPublish(
-                    "", 
-                    QUEUE, 
-                    props, 
-                    mapper.writeValueAsString(req).getBytes("UTF-8")
-                );
+            // Declare the queue if it is not declared yet
+            channel.queueDeclare(QUEUE, true, false, false, null);
 
-                final BlockingQueue<String> resp = new ArrayBlockingQueue<>(1);
+        } catch (Exception e) {
+            LOG.error("Can not initialize EmailSenderRpcClient: ", e);
+            throw e;
+        }
+    }
 
-                var ctag = channel.basicConsume(replyTo, true, (consumerTag, delivery) -> {
-                    if (delivery.getProperties().getCorrelationId().equals(correlationId)) {
-                        resp.offer(new String(delivery.getBody(), "UTF-8"));
-                    }
-                }, consumerTag -> {});
+    public Object exchange(EmailRequest req) {
 
-                result = mapper.readValue(resp.take(), Map.class);
-                channel.basicCancel(ctag);
+        Object result = null;
+
+        try (var conn = factory.newConnection();
+            var channel = conn.createChannel()) {
+            final var correlationId = UUID.randomUUID().toString();
+            final var replyTo = channel.queueDeclare().getQueue();
+            
+            var props = new AMQP.BasicProperties().builder()
+                .correlationId(correlationId)
+                .replyTo(replyTo)
+                .build();
+
+            channel.basicPublish(
+                "", 
+                QUEUE, 
+                props, 
+                mapper.writeValueAsString(req).getBytes(StandardCharsets.UTF_8)
+            );
+
+            final BlockingQueue<String> resp = new ArrayBlockingQueue<>(1);
+
+            var ctag = channel.basicConsume(replyTo, true, (consumerTag, delivery) -> {
+                if (delivery.getProperties().getCorrelationId().equals(correlationId)) {
+                    resp.offer(new String(delivery.getBody(), StandardCharsets.UTF_8));
+                }
+            }, consumerTag -> {});
+
+            result = mapper.readValue(resp.take(), Map.class);
+            channel.basicCancel(ctag);
 
         } catch (Exception e) {
             LOG.error("An error occurred while sending email: ", e);
